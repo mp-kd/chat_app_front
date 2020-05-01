0095e42c36e0c0764edcfc8c09035e1b08ed1aba
Kamil <edgemond2@gmail.com>
2020-05-01 17:03:39 +0200

    Password validator tests

---
+++ test/features/auth/core/password_validator.dart
@@ -0,0 +1,83 @@
+
+import 'package:chat_app_front/features/auth/core/validators.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+  test('PasswordValidator should return null', (){
+    //arrange
+    final String passw = "zaq1@WSX";
+    //act
+    final String result = PasswordValidator.validate(passw);
+    //assert
+    expect(result, null);
+  },);
+
+  test('PasswordValidator should return null', (){
+    //arrange
+    final String passw = "Hwdp!234";
+    //act
+    final String result = PasswordValidator.validate(passw);
+    //assert
+    expect(result, null);
+  },);
+
+
+  test('PasswordValidator should return invalid format', (){
+    //arrange
+    final String passw = "hwdp!234";
+    //act
+    final String result = PasswordValidator.validate(passw);
+    //assert
+    expect(result, PasswordValidator.PASSWORD_INVALID_FORMAT);
+  },);
+
+
+  test('PasswordValidator should return invalid format', (){
+    //arrange
+    final String passw = "asd";
+    //act
+    final String result = PasswordValidator.validate(passw);
+    //assert
+    expect(result, PasswordValidator.PASSWORD_INVALID_FORMAT);
+  },);
+
+
+  test('PasswordValidator should return invalid format', (){
+    //arrange
+    final String passw = "asd12345";
+    //act
+    final String result = PasswordValidator.validate(passw);
+    //assert
+    expect(result, PasswordValidator.PASSWORD_INVALID_FORMAT);
+  },);
+
+
+  test('PasswordValidator should return invalid format', (){
+    //arrange
+    final String passw = "#@!12345";
+    //act
+    final String result = PasswordValidator.validate(passw);
+    //assert
+    expect(result, PasswordValidator.PASSWORD_INVALID_FORMAT);
+  },);
+
+
+  test('PasswordValidator should return invalid format', (){
+    //arrange
+    final String passw = "#s!12345";
+    //act
+    final String result = PasswordValidator.validate(passw);
+    //assert
+    expect(result, PasswordValidator.PASSWORD_INVALID_FORMAT);
+  },);
+
+
+  test('PasswordValidator should return cant be empty', (){
+    //arrange
+    final String passw = "";
+    //act
+    final String result = PasswordValidator.validate(passw);
+    //assert
+    expect(result, PasswordValidator.PASSWORD_CANT_BE_EMPTY);
+  },);
+}
